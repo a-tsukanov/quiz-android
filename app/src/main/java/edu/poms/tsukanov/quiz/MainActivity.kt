2aeb29f558ceb557196156da593f1a8b04bae601
sasha <sasha.ts91@gmail.com>
2018-09-28 20:54:55 +0300

    buttons for quizzes, rm fab

--- app/src/main/java/edu/poms/tsukanov/quiz/MainActivity.kt
+++ app/src/main/java/edu/poms/tsukanov/quiz/MainActivity.kt
@@ -34,10 +34,6 @@ class MainActivity :
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            openFragment(chooseQuizFragment)
-        }
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
